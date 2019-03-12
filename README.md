615b22abd99b4e81fdeeb7200db336f12835d51a
Ramil Mamedov <ramilmamedov@Ramils-MacBook-Pro.local>
2019-03-12 21:41:23 +0400

    1️⃣ commit

---
+++ README.md
@@ -0,0 +1,147 @@
+# BB8 - Starter Kit
+
+Starter kit for automating tasks in everyday front-end development. 👨🏻‍💻 ⚡️ 🛠  ✨ 🤖
+
+## Installation ⬇️
+
+**1.** Make sure you have a recent version of [node.js](https://nodejs.org/en/) installed.
+
+**2.** Install  [yarn](https://yarnpkg.com/en/docs/install).
+
+> Yarn is a new, modern dependency management tool for JavaScript. It caches every package it downloads so it never needs to download it again. The easiest way to install:
+
+```bash
+❯ $ npm install -g yarn
+```
+
+**3.** Clone repository either with SSH or with HTTPS.
+
+```bash
+❯ $ git clone git@github.com:RamilMamedo/BB8.git
+❯ $ git clone https://github.com/RamilMamedo/BB8
+```
+
+**4.** Install [gulp](https://gulpjs.com/).
+
+> Gulp is a toolkit for automating painful or time-consuming tasks in your development workflow, so you can stop messing around and build something. The easiest way to install:
+
+```bash
+❯ $ yarn global add gulp-cli
+```
+
+**5.** Go to the downloaded folder.
+
+```bash
+❯ $ cd BB8
+```
+
+**6.** To get start immediately.
+
+```bash
+❯ $ yarn run start
+```
+> If you don’t want to install or configure tools, plugins or any other dependencies. So that you can focus on the code, just create a project, and you’re good to go.
+
+## Development & Production 🚀
+
+![Dev Mode](https://user-images.githubusercontent.com/38986496/53797621-f8c2e800-3f4f-11e9-9188-80e0c7704fa4.png)
+
+> You can custom setup the project at any time and specify the dependencies that you need in the file **package.json**
+
+**1.**  To installing dependencies manually.
+
+```bash
+❯ $ yarn
+```
+
+**2.** To start development.
+
+```bash
+❯ $ yarn run dev
+```
+> If you did everything correctly, it should open a browser with a local server on port **[2020](http://localhost:2020)** and a working **browser-sync**.
+
+**3.** To finalize the project for production.
+
+```bash
+❯ $ yarn run build
+```
+
+## Folder Structures 📂
+
+The last command will create a directory called **`dist`** inside the current folder.<br>
+Inside that directory, it will generate the initial project structure:
+
+```
+dist
+├── css
+│   └── main.min.css
+├── fonts
+│   └── .woff, .woff2
+├── img
+│   └── .svg, .png, .jpg
+├── js
+│   ├── core.js
+│   └── vendors.min.js
+│
+└── index.html
+```
+
+No configuration or complicated folder structures, just the ready files you need for production.
+
+## Plugins 🔌
+
+![Plugins](https://user-images.githubusercontent.com/38986496/53797620-f8c2e800-3f4f-11e9-8a51-3abf34dcb4ec.png)
+
+- [del](https://www.npmjs.com/package/del) — for deleting files and folders;
+- [browser-sync](https://browsersync.io/docs/gulp) — time-saving synchronized browser testing and live webpage reload when making changes to your project files;
+- [gulp-size](https://www.npmjs.com/package/gulp-size) — Logs out the total size of files in the stream and optionally the individual file-sizes;
+- [gulp-newer](https://www.npmjs.com/package/gulp-newer) — for passing through only those source files that are newer than corresponding destination files;
+- [gulp-debug](https://www.npmjs.com/package/gulp-debug) — debug Vinyl file streams to see what files are run through your Gulp pipeline;
+- [gulp-rename](https://www.npmjs.com/package/gulp-rename) — for renaming files easily, adding suffixes and prefixes;
+- [gulp-concat](https://www.npmjs.com/package/gulp-concat) — for concating files in the order that they are specified in the **gulp.src** function;
+- [gulp-plumber](https://www.npmjs.com/package/gulp-plumber) — prevent pipe breaking caused by errors from gulp plugins;
+
+#### HTML
+- [gulp-rigger](https://www.npmjs.com/package/gulp-rigger) — is a build time include engine for in general any type of text file that you wish to might want to "include" other files into;
+- [gulp-htmlmin](https://www.npmjs.com/package/gulp-htmlmin) — for minifying HTML files;
+- [gulp-html-beautify](https://www.npmjs.com/package/gulp-html-beautify) — for beautifying HTML files;
+
+#### CSS
+- [csso](https://www.npmjs.com/package/csso) —  CSS minifier, cleaner, compressor and restructurer;
+- [cssnano](https://www.npmjs.com/package/cssnano) — is a modern, modular compression tool written on top of the PostCSS ecosystem, which allows us to use a lot of powerful features in order to compact CSS appropriately;
+- [gulp-sass](https://www.npmjs.com/package/gulp-sass) — SCSS to CSS compiler;
+- [css-mqpacker](https://www.npmjs.com/package/css-mqpacker) — packing same CSS media query rules into one using PostCSS;
+- [css-declaration-sorter](https://www.npmjs.com/package/css-declaration-sorter) — sorting CSS declarations based on their property names;
+- [gulp-postcss](https://www.npmjs.com/package/gulp-postcss) — gulp plugin to pipe CSS through several plugins, but parse CSS only once;
+  - for more: [postcss](https://github.com/postcss/postcss);
+- [gulp-stylelint](https://www.npmjs.com/package/gulp-stylelint) — a mighty, modern linter that helps you avoid errors and enforce conventions in your styles;
+  - for more: [stylelint](https://stylelint.io/);
+- [gulp-purifycss](https://www.npmjs.com/package/gulp-purifycss) — for removing unused styles;
+  - for more: [purifycss](https://github.com/purifycss/purifycss);
+- [gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer) — automatically places vendor prefixes in CSS according to the [Can I Use](https://caniuse.com/) service;
+  - for more: [autoprefixer](https://github.com/postcss/autoprefixer/);
+
+#### JS
+- [gulp-uglify](https://www.npmjs.com/package/gulp-uglify) — for minifying JavaScript with UglifyJS3;
+- [gulp-babel](https://www.npmjs.com/package/gulp-babel) — to support modern JS (ES6) in browsers;
+  - for more: [babel](https://babeljs.io/);
+- [gulp-eslint](https://www.npmjs.com/package/gulp-eslint) — identifying and reporting on patterns found in ECMAScript/JavaScript code;
+  - for more: [eslint](https://eslint.org/);
+
+#### IMG
+- [gulp-webp](https://www.npmjs.com/package/gulp-webp) — converting images into a modern format WebP.
+- [gulp-favicons](https://github.com/evilebottnawi/favicons) — favicons generator based on node.js;
+- [gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin) — image compression PNG, JPG, GIF and SVG (including additional plugins for optimization);
+  - for more: [imagemin](https://github.com/imagemin/imagemin);
+- [gulp-svg-sprites](https://www.npmjs.com/package/gulp-svg-sprites) — for creating SVG sprites;
+
+> Read more about image optimization here: [[imagemin-to-compress-images](https://web.dev/fast/use-imagemin-to-compress-images), [imagemin-plugins-difference](http://pointlessramblings.com/posts/pngquant_vs_pngcrush_vs_optipng_vs_pngnq)];
+>
+> Read more about webfont optimization here: [Web Font Optimization](https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/webfont-optimization);
+>
+> Simple [online tool](https://transfonter.org/) for generating CSS @font-face and transforming difference font formats;
+
+## License
+
+The code is available under the [MIT license](https://github.com/RamilMamedo/BB8/blob/master/LICENCE).
---
+++ gulpfile.babel.js
@@ -0,0 +1,303 @@
+'use strict';
+
+//  This gulpfile makes use of new JavaScript features.
+//  Babel handles this without us having to do anything. It just works.
+
+//  Required Modules
+import del from 'del';
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import imagemin from 'gulp-imagemin';
+import browserSync from 'browser-sync';
+import plugins from 'gulp-load-plugins';
+
+//  You can choose whether to use Dart Sass or Node Sass by setting the sass.compiler property.
+//  You can read more about sass compilers here: https://www.npmjs.com/package/gulp-sass
+sass.compiler = require('node-sass');
+
+//  Global constants
+const $ = plugins(),
+  reload = browserSync.reload,
+  paths = {
+    app: {
+      root: './app/',
+      html: {
+        main: './app/*.html',
+        all: './app/**/*.html'
+      },
+      styles: {
+        main: './app/sass/main.scss',
+        all: './app/sass/**/*.scss'
+      },
+      scripts: {
+        main: './app/js/core.js',
+        all: './app/js/**/*.js',
+        libs: ['./app/js/lib/jquery.min.js']
+      },
+      images: {
+        main: './app/img/**/*',
+        favicon: './app/credentials/favicon.png',
+        svgIcons: './app/img/svg-icons/*.svg'
+      },
+      fonts: './app/fonts/**/*'
+    },
+    dist: {
+      root: './dist/',
+      js: './dist/js/',
+      css: './dist/css/',
+      img: {
+        main: './dist/img/',
+        favicons: './dist/credentials/favicons/'
+      },
+      fonts: './dist/fonts/'
+    }
+  };
+
+//  Removing production directory.
+export const clear = () => {
+  return del(paths.dist.root);
+};
+
+/**
+ * Live reload and time-saving synchronized browser testing.
+ * For more about options of BrowserSync: {@link https://browsersync.io/docs/api}
+ **/
+export const server = done => {
+  browserSync.init({
+    server: {
+      baseDir: paths.dist.root
+    },
+    port: 2020,
+    open: true,
+    notify: false
+//  https: true,
+//  tunnel: true,
+//  tunnel: 'BB8' //  Demonstration page: http://BB8.localtunnel.me
+  });
+  done();
+};
+
+//  Moving fonts to the production directory.
+export const fonts = () => {
+  return gulp
+    .src(paths.app.fonts, { since: gulp.lastRun(fonts) })
+    .pipe($.plumber())
+    .pipe($.newer(paths.dist.fonts))
+    .pipe(gulp.dest(paths.dist.fonts))
+    .pipe($.size({ title: 'Font Size:' }))
+    .pipe($.debug({ title: 'Font Files:' }))
+    .on('end', reload);
+};
+
+/**
+ * Optimizing images.
+ * For more about used image plugins here: {@link https://bit.ly/2XPqEin#img}
+ **/
+export const img = () => {
+  const svgOptions = {
+    removeViewBox: false,
+    collapseGroups: true,
+    removeComments: true,
+    removeEmptyAttrs: true,
+    removeEmptyText: true,
+    removeUnusedNS: true,
+    collapseGroups: true
+  };
+  const webpOptions = {
+    lossless: true,
+    quality: 70,
+    alphaQuality: 90
+  };
+  return (
+    gulp
+      .src(paths.app.images.main, { since: gulp.lastRun(img) })
+      .pipe($.plumber())
+      .pipe($.newer(paths.dist.img.main))
+      .pipe(
+        imagemin([
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.jpegtran({ progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.svgo({ plugins: [svgOptions] })
+        ])
+      )
+//    .pipe($.webp(webpOptions))  // (Optional) enable if you want to convert images to WebP format.
+      .pipe(gulp.dest(paths.dist.img.main))
+      .pipe($.size({ title: 'Image Size:' }))
+      .pipe($.debug({ title: 'Image File:' }))
+      .on('end', reload)
+  );
+};
+
+/**
+ * Generating favicons.
+ * For more about options: {@link https://github.com/itgalaxy/favicons}
+ **/
+export const favicons = () => {
+  const settings = {
+    appName: 'BB8',
+    appShortName: 'BB8',
+    appDescription:
+      'Starter kit for automating tasks in everyday front-end development.',
+    developerName: 'Ramil Mamedov',
+    dir: 'ltr',
+    lang: 'en-US',
+    background: '#fff',
+    theme_color: '#fff',
+    appleStatusBarStyle: 'black-translucent',
+    icons: {
+      android: true,
+      windows: true,
+      favicons: true,
+      appleIcon: true,
+      coast: false,
+      yandex: false,
+      firefox: false,
+      appleStartup: false
+    }
+  };
+  return gulp
+    .src(paths.app.images.favicon, { since: gulp.lastRun(favicons) })
+    .pipe($.plumber())
+    .pipe($.newer(paths.dist.img.favicons))
+    .pipe($.favicons(settings))
+    .pipe($.size({ title: 'Favicon Size:' }))
+    .pipe($.debug({ title: 'Favicon Files:' }))
+    .pipe(gulp.dest(paths.dist.img.favicons));
+};
+
+/**
+ * Creating SVG sprites.
+ * For more about options: {@link https://github.com/jkphl/svg-sprite}
+ **/
+export const svgSprites = () => {
+  const config = {
+    dest: './app/sprites/',
+    shape: {
+      // Set maximum dimensions
+      dimension: {
+        maxWidth: 32,
+        maxHeight: 32
+      },
+      // Add padding
+      spacing: {
+        padding: 10
+      }
+    },
+    mode: {
+      css: {
+        dest: './sprites/',
+        sprite: 'sprite.svg',
+        bust: false,
+        dimensions: true,
+        render: {
+          scss: true
+        }
+      }
+    }
+  };
+  return gulp
+    .src(paths.app.images.svgIcons)
+    .pipe($.plumber())
+    .pipe($.svgSprite(config))
+    .pipe($.size({ title: 'SVG Sprite Size:' }))
+    .pipe($.debug({ title: 'SVG Sprite File:' }))
+    .pipe(gulp.dest(paths.app.root));
+};
+
+/**
+ * Concatenation partials of HTML files and minify-beautify them by choice.
+ * For more about used html here: {@link https://bit.ly/2XPqEin#html}
+ **/
+export const html = () => {
+  return (
+    gulp
+      .src(paths.app.html.main)
+      .pipe($.plumber())
+      .pipe($.rigger())
+      .pipe($.htmlBeautify({ indent_size: 2, preserve_newlines: false }))
+//    .pipe($.htmlmin({ collapseWhitespace: true }))  // (Optional) enable if you want to minify html files for production.
+      .pipe(gulp.dest(paths.dist.root))
+      .pipe($.size({ title: 'HTML Size:' }))
+      .pipe($.debug({ title: 'HTML File:' }))
+      .on('end', reload)
+  );
+};
+
+/**
+ * Compiling SCSS, passing it through PostCSS plugins, cleaning unnecessary CSS parts, minifying and renaming the final file.
+ * For more about used css here: {@link https://bit.ly/2XPqEin#css}
+ **/
+export const css = () => {
+  const styleLintSetting = {
+      debug: true,
+      reporters: [{ formatter: 'string', console: true }]
+    },
+    plugins = [
+      require('autoprefixer')({ browsers: ['last 3 version'] }),
+      require('css-mqpacker')({ sort: true }),
+      require('cssnano')({ safe: true }),
+      require('css-declaration-sorter')({ order: 'smacss' })
+    ];
+  return (
+    gulp
+      .src(paths.app.styles.main)
+      .pipe($.plumber())
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+      .pipe($.purifycss([paths.app.scripts.main, paths.app.html.all])) // (Optional) disable if you don't want to cut unused CSS.
+      .pipe($.postcss(plugins)) // (Optional) disable if you don't want to use PostCSS plugins.
+      .pipe($.csso())
+      .pipe($.rename({ suffix: '.min' }))
+      .pipe(gulp.dest(paths.dist.css))
+//    .pipe($.stylelint(styleLintSetting)) // (Optional) enable if you need to lint final CSS file.
+      .pipe($.size({ title: 'CSS Size:' }))
+      .pipe($.debug({ title: 'CSS File:' }))
+      .on('end', reload)
+  );
+};
+
+/**
+ * Compiling core script file with Babel.
+ * For more about used css here: {@link https://bit.ly/2XPqEin#js}
+ **/
+export const js = () => {
+  return gulp
+    .src(paths.app.scripts.main)
+    .pipe($.plumber())
+    .pipe($.babel({ presets: ['@babel/preset-env'] }))
+    .pipe(gulp.dest(paths.dist.js))
+//  .pipe($.eslint({ configFile: '.eslintrc.json' }))  // (Optional) enable if you need to lint core JS file.
+//  .pipe($.eslint.format())  // (Optional) enable if you need to lint core JS file.
+    .pipe($.size({ title: 'JS Size:' }))
+    .pipe($.debug({ title: 'JS Files:' }))
+    .on('end', reload);
+};
+
+//  Concatenating JavaScript libraries & minifying the final file.
+export const jsLibs = () => {
+  return gulp
+    .src(paths.app.scripts.libs)
+    .pipe($.plumber())
+    .pipe($.uglify()) // (Optional) disable if you don't want to minify-uglify JS vendors.
+    .pipe($.concat('vendors.min.js'))
+    .pipe(gulp.dest(paths.dist.js))
+    .pipe($.size({ title: 'JS Libs Size:' }))
+    .pipe($.debug({ title: 'JS Libs Files:' }))
+    .on('end', reload);
+};
+
+//  Watching HTML, CSS, JS & media files for changes.
+export const watchFiles = () => {
+  gulp.watch(paths.app.images.main, img);
+  gulp.watch(paths.app.styles.all, css);
+  gulp.watch(paths.app.scripts.all, javascript);
+  gulp.watch(paths.app.html.all, html);
+};
+
+//  Export Complex Tasks
+export const javascript = gulp.series(jsLibs, js);
+export const watch = gulp.parallel(watchFiles, server);
+export const main = gulp.parallel(css, img, fonts, html, javascript);
+export const build = gulp.series(clear, main);
+export const dev = gulp.series(main, watch);
+exports.default = dev;
